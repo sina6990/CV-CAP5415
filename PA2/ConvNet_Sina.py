c00bfb4c987a0cbd3fd2396380dab6c4ddf4c6c9
Sina Abdidizaji <sinaabdi.d@gmail.com>
2024-10-19 15:34:31 -0400

    managing assignment folders

---
+++ PA2/ConvNet_Sina.py
@@ -0,0 +1,94 @@
+import time
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ConvNet(nn.Module):
+    def __init__(self, mode):
+        super(ConvNet, self).__init__()
+        
+        # Define various layers here, such as in the tutorial example
+        # self.conv1 = nn.Conv2D(...)
+        
+        # This will select the forward pass function based on mode for the ConvNet.
+        # Based on the question, you have 5 modes available for step 1 to 5.
+        # During creation of each ConvNet model, you will assign one of the valid mode.
+        # This will fix the forward function (and the network graph) for the entire training/testing
+        if mode == 1:
+            self.forward = self.model_1
+        elif mode == 2:
+            self.forward = self.model_2
+        elif mode == 3:
+            self.forward = self.model_3
+        elif mode == 4:
+            self.forward = self.model_4
+        elif mode == 5:
+            self.forward = self.model_5
+        else: 
+            print("Invalid mode ", mode, "selected. Select between 1-5")
+            exit(0)
+        
+        
+    # Baseline model. step 1
+    def model_1(self, X):
+        # ======================================================================
+        # One fully connected layer.
+        #
+        # ----------------- YOUR CODE HERE ----------------------
+        #
+        # Uncomment the following return stmt once method implementation is done.
+        # return  fcl
+        # Delete line return NotImplementedError() once method is implemented.
+        return NotImplementedError()
+
+    # Use two convolutional layers.
+    def model_2(self, X):
+        # ======================================================================
+        # Two convolutional layers + one fully connnected layer.
+        #
+        # ----------------- YOUR CODE HERE ----------------------
+        #
+        # Uncomment the following return stmt once method implementation is done.
+        # return  fcl
+        # Delete line return NotImplementedError() once method is implemented.
+        return NotImplementedError()
+
+    # Replace sigmoid with ReLU.
+    def model_3(self, X):
+        # ======================================================================
+        # Two convolutional layers + one fully connected layer, with ReLU.
+        #
+        # ----------------- YOUR CODE HERE ----------------------
+        #
+        # Uncomment the following return stmt once method implementation is done.
+        # return  fcl
+        # Delete line return NotImplementedError() once method is implemented.
+        return NotImplementedError()
+
+    # Add one extra fully connected layer.
+    def model_4(self, X):
+        # ======================================================================
+        # Two convolutional layers + two fully connected layers, with ReLU.
+        #
+        # ----------------- YOUR CODE HERE ----------------------
+        #
+        # Uncomment the following return stmt once method implementation is done.
+        # return  fcl
+        # Delete line return NotImplementedError() once method is implemented.
+        return NotImplementedError()
+
+    # Use Dropout now.
+    def model_5(self, X):
+        # ======================================================================
+        # Two convolutional layers + two fully connected layers, with ReLU.
+        # and  + Dropout.
+        #
+        # ----------------- YOUR CODE HERE ----------------------
+        #
+
+        # Uncomment the following return stmt once method implementation is done.
+        # return  fcl
+        # Delete line return NotImplementedError() once method is implemented.
+        return NotImplementedError()
+    
+    
